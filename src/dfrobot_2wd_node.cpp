abab06a6b6d41705132891e585b868864fcfe275
Simone Nardi <monena@tiscali.it>
2015-07-29 19:27:12 +0200

    get the robot_name from launch file, if it exist.

--- src/dfrobot_2wd_node.cpp
+++ src/dfrobot_2wd_node.cpp
@@ -8,7 +8,18 @@
 int main ( int argc, char** argv )
 {
     //TODO ros param robot name
-    std::string robot_name="pippo";
+      std::string robot_name;
+      ros::NodeHandle l_node("~");
+      if (l_node.getParam("robot_name", robot_name))
+      {
+	ROS_INFO("Robot name received: %s", robot_name.c_str());
+      }
+      else
+      {
+	robot_name="red_blue";
+	ROS_INFO("Robot name NOT received: %s", robot_name.c_str());
+      }
+  
     ros::init(argc,argv,robot_name);
     ros_real_world_serial_communicator comm(robot_name);
     ros::spin();
