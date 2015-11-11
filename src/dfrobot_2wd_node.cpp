d965dde31fab49967b965aa9e85199616f878084
Simone Nardi <monena@tiscali.it>
2015-11-11 19:07:00 +0100

    modified subscriber topic name: /robot_name/cmd_vel

--- src/dfrobot_2wd_node.cpp
+++ src/dfrobot_2wd_node.cpp
@@ -8,17 +8,19 @@
 int main ( int argc, char** argv )
 {
     //TODO ros param robot name
-      std::string robot_name;
-      ros::NodeHandle l_node("~");
-      if (l_node.getParam("robot_name", robot_name))
-      {
-	ROS_INFO("Robot name received: %s", robot_name.c_str());
-      }
-      else
-      {
-	robot_name="red_blue";
-	ROS_INFO("Robot name NOT received: %s", robot_name.c_str());
-      }
+    std::string robot_name;
+    ros::NodeHandle l_node("~");
+    if (l_node.getParam("robot_name", robot_name))
+    {
+      ROS_INFO("Robot name received: %s", robot_name.c_str());
+    }
+    else
+    {
+      robot_name="red_blue";
+      ROS_INFO("Robot name NOT received: %s", robot_name.c_str());
+    }
+  
+    robot_name = "/"+robot_name+"/cmd_vel";
   
     ros::init(argc,argv,robot_name);
     ros_real_world_serial_communicator comm(robot_name);
